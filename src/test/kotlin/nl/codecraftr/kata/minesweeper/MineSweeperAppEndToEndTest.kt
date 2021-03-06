4657c93f73729215dcd197a5b6f6cd7d8bb57371
Roy Straub <roy.straub@codesquad.nl>
2021-03-06 13:14:32 +0100

    Full acceptance test

--- src/test/kotlin/nl/codecraftr/kata/minesweeper/MineSweeperAppEndToEndTest.kt
+++ src/test/kotlin/nl/codecraftr/kata/minesweeper/MineSweeperAppEndToEndTest.kt
@@ -6,18 +6,33 @@ import io.kotest.matchers.shouldBe
 internal class MineSweeperAppEndToEndTest : BehaviorSpec({
     val mineSweeperApp = MineSweeperApp(NoopInputOutputParser, TextMineSweeperPresenter())
 
-    given("a single minefield with a single square and row") {
+    given("unsolved minesweeper notation") {
         `when`("it is solved") {
-            then("it should return the solved minefield") {
+            then("it should return the solved minesweeper notation") {
                 val input = """
-                    1 1
-                    .
+                    4 4
+                    *...
+                    ....
+                    .*..
+                    ....
+                    3 5
+                    **...
+                    .....
+                    .*...
                     0 0
                 """.trimIndent()
 
                 val expected = """
                     Field #1:
-                    0
+                    *100
+                    2210
+                    1*10
+                    1110
+
+                    Field #2:
+                    **100
+                    33200
+                    1*100
                 """.trimIndent()
 
                 val result = mineSweeperApp.solve(input)
