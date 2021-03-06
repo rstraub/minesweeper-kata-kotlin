93832a1b177f6b5123a1b6c4447830d81e28b8b0
Roy Straub <roy.straub@codesquad.nl>
2021-03-06 11:22:22 +0100

    Simpler e2e test

--- src/test/kotlin/nl/codecraftr/kata/minesweeper/MineSweeperAppEndToEndTest.kt
+++ src/test/kotlin/nl/codecraftr/kata/minesweeper/MineSweeperAppEndToEndTest.kt
@@ -3,31 +3,7 @@ package nl.codecraftr.kata.minesweeper
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
-internal class MineSweeperAppAcceptanceTest : BehaviorSpec({
-    given("an empty minefield") {
-        `when`("it is solved") {
-            then("it should return zero solved minefields") {
-                val input = ""
-
-                val result = MineSweeperApp().solve(input)
-
-                result shouldBe ""
-            }
-        }
-    }
-
-    given("end of input") {
-        `when`("it is solved") {
-            then("it should return zero solved minefields") {
-                val input = "0 0"
-
-                val result = MineSweeperApp().solve(input)
-
-                result shouldBe ""
-            }
-        }
-    }
-
+internal class MineSweeperAppEndToEndTest : BehaviorSpec({
     given("a single minefield") {
         `when`("it is solved") {
             then("it should return the solved minefield") {
